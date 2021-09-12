223bb4055d5eb07718bc3f019fb8486511e654a7
sandeep99489 <86335975+sandeep99489@users.noreply.github.com>
2021-09-12 19:21:17 +1000

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('sandeep rocks!'));
+app.get('/', (req, res) => res.send('sandeep rjcks!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
